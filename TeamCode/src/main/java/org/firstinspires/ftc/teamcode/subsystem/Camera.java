ddca76859279d61bd0e833128de628140b76e87b
Aidan <mahamoti+eaglebots22032_1@gmail.com>
2023-11-13 15:29:03 -0600

    Camera Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Camera.java
@@ -1,5 +1,24 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
-public class Camera {
+import android.content.Intent;
 
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import java.util.List;
+
+public class Camera {
+    Intent intent = new Intent ("android.media.action.IMAGE_CAPTURE"); startActivity (intent);
+    TfodProcessor tfodProcesser = TfodProcessor.easyCreateWithDefaults();
+    VisionPortal visionPortal = VisionPortal.easyCreateWithDefaults(Camera, TfodProcessor);
+    List<Recognition> recognitions = tfodProcesser .getRecognitions();
+    for (Recognition recognition : recognitions)
 }
+
+String label = recognition.getLabel();
+
+float confidence = recognition.getConfidence();
+
+telemetry.addline("Recognized object: " + label);
+telemetry.addline("Confidence: " + confidence);
