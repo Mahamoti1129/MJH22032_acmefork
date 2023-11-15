20c2a2d57bc8800925858172e3588b49de631024
Owen <mahamoti+eaglebots22032_2@gmail.com>
2023-11-15 15:30:01 -0600

    Add in the starting positions for the autonomus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueBackstageAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueBackstageAutonomous.java
@@ -9,16 +9,18 @@ import org.firstinspires.ftc.teamcode.subsystem.Robot;
 
 @Autonomous(name="Blue Backstage Auto", group="Autonomous")
 public class BlueBackstageAutonomous extends LinearOpMode {
+
+    private final Pose2d STARTING_POSITION = new Pose2d(-65,12, Math.toRadians(90));
     @Override
     public void runOpMode() throws InterruptedException {
 //        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-        Robot gRex = new Robot(hardwareMap, new Pose2d(0, 0, 0));
+        Robot gRex = new Robot (hardwareMap, STARTING_POSITION);
 
         // replaces waitForStart() and enables camera searching before runtime
         while (!isStarted() && !isStopRequested()){
         }
 
-        gRex.actionBuilder(new Pose2d(0, 0, 0))
+        gRex.drive.actionBuilder(STARTING_POSITION)
                 .lineToX(3) // get off of wall
                 .lineToY(24) // park backstage
                 .build();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedBackstageAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedBackstageAutonomous.java
@@ -9,16 +9,18 @@ import org.firstinspires.ftc.teamcode.subsystem.Robot;
 
 @Autonomous(name="Red Backstage Auto", group="Autonomous")
 public class RedBackstageAutonomous extends LinearOpMode {
+
+    private final Pose2d STARTING_POSITION = new Pose2d(65,12,Math.toRadians(270));
     @Override
     public void runOpMode() throws InterruptedException {
       //  MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-        Robot gRex = new Robot(hardwareMap, new Pose2d(0, 0, 0));
+        Robot gRex = new Robot(hardwareMap, STARTING_POSITION);
 
         // replaces waitForStart() and enables camera searching before runtime
         while (!isStarted() && !isStopRequested()){
         }
 
-        gRex.actionBuilder(new Pose2d(0, 0, 0))
+        gRex.drive.actionBuilder(STARTING_POSITION)
                 .lineToX(3) // get off of wall
                 .lineToY(24) // park backstage
                 .build();
