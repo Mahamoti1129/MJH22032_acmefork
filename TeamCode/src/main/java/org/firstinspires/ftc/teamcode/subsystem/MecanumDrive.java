dcbad27b73a4d09bbab6c48bacb6ad8c2f7dd7f7
Aidan <mahamoti+eaglebots22032_1@gmail.com>
2024-02-17 13:57:16 -0600

    Camera Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/MecanumDrive.java
@@ -64,9 +64,9 @@ public final class MecanumDrive {
         public double kA = 0;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 100;
+        public double maxWheelVel = 250;
         public double minProfileAccel = -30;
-        public double maxProfileAccel = 75;
+        public double maxProfileAccel = 150;
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
