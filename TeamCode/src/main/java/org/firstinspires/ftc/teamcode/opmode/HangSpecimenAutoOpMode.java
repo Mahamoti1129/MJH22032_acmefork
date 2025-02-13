5b368134a135d601c02cd93a28815fe59187609a
James May <jamesmay@lena.org>
2025-02-13 16:33:42 -0600

    first auto: hang specimen only

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/HangSpecimenAutoOpMode.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.TheRobot;
+import org.firstinspires.ftc.teamcode.commands.LongParkOnlyCommand;
+
+@Autonomous(name="HangSpecimen", group="Auto")
+public class HangSpecimenAutoOpMode extends CommandOpMode {
+
+    private TheRobot robot;
+
+    private final Pose2d START_POSITION = new Pose2d(-9, -63, Math.PI/2);
+
+    @Override
+    public void initialize() {
+        robot = new TheRobot(hardwareMap, gamepad1, gamepad2, telemetry);
+        telemetry.addData("Robot Status", "Initializing Hang Specimen OpMode");
+
+        schedule(new InstantCommand(() -> {
+            robot.drivetrain.actionBuilder(START_POSITION)
+                    .lineToY(-39)
+                    .build();
+        }));
+
+        // raise arm, extend arm
+
+        schedule(new InstantCommand(() -> {
+            robot.drivetrain.actionBuilder(START_POSITION)
+                    .lineToY(-33)
+                    .build();
+        }));
+
+        // open claw
+
+    }
+}
